a33ccb5a2bc52520cabce1b85e8b0f2dc72a16cf
Jason Chan <shunsei.chan@gmail.com>
2022-05-22 11:08:57 -0700

    Initial Movement Done

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour
+internal class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _jumpPower = 3.5f;
+    [SerializeField] private float _speed = 3f;
+    private float _x, _y;
+    private bool _canJump = false;
+    private Rigidbody2D _rb;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            _canJump = true;
+            return;
+        }
+        _canJump = false;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_x * _speed, _rb.velocity.y);
+        if (!_canJump) return;
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpPower);
     }
 }
